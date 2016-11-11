107dcc7320bd0289be7ea421075777148c6e2302
serrigo <silvana.errigo@gmail.com>
2016-11-11 01:45:11 -0500

    Modelo Cotizaciones
    Se agrega este archivo para previsualizar como va quendado el formulario de Cotizaciones.

---
+++ application/models/Cotizaciones_Model.php
@@ -0,0 +1,14 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Cotizaciones_Model extends CI_Model
+{
+	function __construct()
+	{
+		// Call the Model constructor
+		parent::__construct();
+	}
+	
+	
+}
+?>
